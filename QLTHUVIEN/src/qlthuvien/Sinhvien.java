03432ef38adf401197a7fe602abb5994800a91cc
Hữu Thịnh Trần <huuthinhtran@Huus-MacBook-Pro.local>
2023-05-10 20:56:26 +0700

    first commit

---
+++ QLTHUVIEN/src/qlthuvien/Sinhvien.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qlthuvien;
+
+/**
+ *
+ * @author Admin
+ */
+public class Sinhvien {
+    public static String mssv="";
+
+    public Sinhvien() {
+    }
+
+    public static String getMssv() {
+        return mssv;
+    }
+
+    public static void setMssv(String mssv) {
+        Sinhvien.mssv = mssv;
+    }
+    
+}
